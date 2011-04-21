2d523b2bef3a4300d038fd0f9d85e3a4b0da1fa0
sofitaa <sofitaa@6623b716-71eb-9dcb-7a15-08f1f083b448>
2011-04-21 20:14:16 +0000

    Sofia probando el commit :)

---
+++ flexible-manufacturing-cell/src/flexiblemanufacturingcell/resources/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flexiblemanufacturingcell.resources;
+
+/**
+ *
+ * @author Sofi
+ */
+public class Prueba {
+
+}
