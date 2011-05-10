1a186b7a87d58c388e01f35740e3b97f1b5c4ab7
empanadamental <empanadamental@6623b716-71eb-9dcb-7a15-08f1f083b448>
2011-05-10 00:24:34 +0000

    Voy empezando con las cintas y los sensores

--- trunk/flexible-manufacturing-cell/src/Element/Other/Sensor.java
+++ trunk/flexible-manufacturing-cell/src/Element/Other/Sensor.java
@@ -2,42 +2,65 @@
  * This class represents a sensor. It is inserted in a container and activated when a piece
  * reaches a certain position.
  */
-
 package Element.Other;
 
+import Element.Conveyor.ConveyorBelt;
+import Element.Piece.Piece;
 import Element.PieceContainer;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Sensor {
-	private int _state;
-	private PieceContainer _associatedContainer;
-	private int _positionInBelt;
-	private SensorMailBox _mailBox;
+public class Sensor extends Thread{
 
-	public void getState() {
-		throw new UnsupportedOperationException();
-	}
+    private int _state;
+    private PieceContainer _associatedContainer;
+    private int _positionInBelt;
+    private SensorMailBox _mailBox;
 
-	public void activate() {
-		throw new UnsupportedOperationException();
-	}
+    public int getSensorState() {
+        return _state;
+    }
+    
+    public void run(){
+        
+        // TODO: Check this works right
+        while(true){
+            try {
+                Thread.sleep(50);
+                List<Piece> pieces = _associatedContainer.getPieces();
+                for(Piece p:pieces){
+                    if(p.getPosition() == _positionInBelt){
+                        activate();
+                    }
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ConveyorBelt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 
-	public int getPositionInBelt() {
-		throw new UnsupportedOperationException();
-	}
+    public void activate() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setPositionInBelt(int positionInBelt) {
-		throw new UnsupportedOperationException();
-	}
+    public int getPositionInBelt() {
+        throw new UnsupportedOperationException();
+    }
 
-	public PieceContainer getAssociatedContainer() {
-		throw new UnsupportedOperationException();
-	}
+    public void setPositionInBelt(int positionInBelt) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setAssociatedContainer(PieceContainer associatedContainer) {
-		throw new UnsupportedOperationException();
-	}
+    public PieceContainer getAssociatedContainer() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void notifyContainer() {
-		throw new UnsupportedOperationException();
-	}
+    public void setAssociatedContainer(PieceContainer associatedContainer) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void notifyContainer() {
+        throw new UnsupportedOperationException();
+    }
 }
