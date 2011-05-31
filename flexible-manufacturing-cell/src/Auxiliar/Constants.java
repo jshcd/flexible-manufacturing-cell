ef6fb47647e84f7e72536c844b8b5b6b54877b79
sofitaa@gmail.com <sofitaa@gmail.com@6623b716-71eb-9dcb-7a15-08f1f083b448>
2011-05-31 10:44:56 +0000

    Cambiando resolucion de GUI

--- flexible-manufacturing-cell/src/Auxiliar/Constants.java
+++ flexible-manufacturing-cell/src/Auxiliar/Constants.java
@@ -142,8 +142,8 @@ public class Constants {
     /**
      * constants used for GUI
      */
-    public static final int CANVAS_WIDTH = 1100;
-    public static final int CANVAS_HEIGHT = 540;
+    public static final int CANVAS_WIDTH = 1000;
+    public static final int CANVAS_HEIGHT = 490;
     public static final Point QUALITY_POSITION = new Point(505, 160);  //probando
     public static final String TITLE = "Flexible Manufacturing Cell";
     public static final String CONTROL_PANEL_TITLE = "Control Panel";
