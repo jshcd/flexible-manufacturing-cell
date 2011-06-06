400a39cdf161501499731acf0cde15156e6f168a
ringare@gmail.com <ringare@gmail.com@6623b716-71eb-9dcb-7a15-08f1f083b448>
2011-06-06 20:23:28 +0000

    otro commit jugoso

--- flexible-manufacturing-cell/src/Scada/Gui/Connections.java
+++ flexible-manufacturing-cell/src/Scada/Gui/Connections.java
@@ -66,21 +66,14 @@ public class Connections extends JPanel {
         //if assembly station connected{
         if (connected && automatonID == Constants.SLAVE1_ID) {
             _assemblyStatus.setIcon(new ImageIcon(_imageLoader._on));
-        } else {
-            _assemblyStatus.setIcon(new ImageIcon(_imageLoader._off));
-        }
+        } 
         //} else if welding station connected {
         if (connected && automatonID == Constants.SLAVE2_ID) {
             _weldingStatus.setIcon(new ImageIcon(_imageLoader._on));
-        } else {
-            _weldingStatus.setIcon(new ImageIcon(
-                    _imageLoader._off));
-        }
+        } 
         //} else if welding station connected {
         if (connected && automatonID == Constants.SLAVE3_ID) {
             _qualityStatus.setIcon(new ImageIcon(_imageLoader._on));
-        } else {
-            _qualityStatus.setIcon(new ImageIcon(_imageLoader._off));
         }
     }
 }
