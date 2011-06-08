7f97a370b753034d8db28c913914fb68bf7d6a0a
ringare@gmail.com <ringare@gmail.com@6623b716-71eb-9dcb-7a15-08f1f083b448>
2011-06-08 21:29:02 +0000

    more comments

--- trunk/flexible-manufacturing-cell/src/Element/Station/AssemblyStation.java
+++ trunk/flexible-manufacturing-cell/src/Element/Station/AssemblyStation.java
@@ -1,33 +1,63 @@
-/*
- * This class assambles 2 pieces into a new one
- */
+
 package Element.Station;
 
 import Automaton.Slaves.Slave;
 import Auxiliar.Constants;
 import Element.Other.Sensor;
 import Element.Piece.Piece;
-import Element.Conveyor.ConveyorBelt;
 import Element.Piece.Piece.PieceType;
 import Element.PieceContainer;
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This class represents the Assembly Station that assembles two pieces
+ * @author Echoplex
+ */
 public class AssemblyStation implements PieceContainer {
 
+    /**
+     * Time to assemble pieces
+     */
     private int _assemblyTime;
+    
+    /**
+     * List of pieces in the station
+     */
     protected List<Piece> _pieces;
+    
+    /**
+     * Identifier
+     */
     private int _id;
+    
+    /**
+     * Process that makes the callback
+     */
     protected Slave _process;
+    
+    /**
+     * Shows if the assembly station is active or not
+     */
     protected boolean _moving;
+    
+    /**
+     * Shows if the assembly station is currently working
+     */
     private boolean _actuating;
+    
+    /**
+     * Logger callback
+     */
     protected Logger _logger = Logger.getLogger(AssemblyStation.class.toString());
 
+    /**
+     * Constructor of the class
+     * @param id Identifier
+     */
     public AssemblyStation(int id) {
         _id = id;
         _moving = false;
@@ -35,6 +65,9 @@ public class AssemblyStation implements PieceContainer {
         _pieces = Collections.synchronizedList(new ArrayList<Piece>());
     }
 
+    /**
+     * Defines the Running method of the class
+     */
     @Override
     public synchronized void run() {
         while (true) {
@@ -54,6 +87,10 @@ public class AssemblyStation implements PieceContainer {
         }
     }
 
+    /**
+     * This function creates an assembled piece from a gear and an axis
+     * @return TRUE if everything went good, FALSE in other case
+     */
     public boolean assemble() {
         synchronized (_pieces) {
             if (_pieces.size() == 2) {
@@ -81,30 +118,54 @@ public class AssemblyStation implements PieceContainer {
         }
     }
 
+    /**
+     * @return The time to assembly pieces
+     */
     public int getAssemblyTime() {
         return _assemblyTime;
     }
 
+    /**
+     * Sets a new value for the assembly time
+     * @param assemblyTime New value for the asssembly time
+     */
     public void setAssemblyTime(int assemblyTime) {
         this._assemblyTime = assemblyTime;
     }
 
+    /**
+     * Adds a piece to the station and updates its position
+     * @param p New Piece
+     */
     public void addPiece(Piece p) {
         _pieces.add(p);
         updatePosition(p);
     }
 
+    /**
+     * Adds a sensor to the station
+     * @param s New Sensor
+     */
     public void addSensor(Sensor s) {
     }
 
+    /**
+     * @return LIst of pieces contained in the station
+     */
     public List<Piece> getPieces() {
         return _pieces;
     }
 
+    /**
+     * @return Status of the station
+     */
     public boolean isMoving() {
         return _moving;
     }
 
+    /**
+     * Removes the last pieces added to the station
+     */
     public void removeLastPiece() {
         if (_pieces.isEmpty()) {
             Logger.getLogger(AssemblyStation.class.toString()).log(Level.SEVERE, "Assembly station with id {0}: unable to remove last element", _id);
@@ -113,30 +174,51 @@ public class AssemblyStation implements PieceContainer {
         _pieces.remove(0);
     }
 
+    /**
+     * @return The identifier
+     */
     @Override
     public int getId() {
         return _id;
     }
 
+    /**
+     * Sets a new value to the identifier
+     * @param id New identifier value
+     */
     @Override
     public void setId(int id) {
         _id = id;
     }
 
+    /**
+     * Sets the list of pieces contained in the assembly station
+     * @param pieces New list of pieces
+     */
     public void setPieces(List<Piece> pieces) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * @return The process linked to the station
+     */
     @Override
     public Slave getProcess() {
         return _process;
     }
 
+    /**
+     * Sets a new process to the station
+     * @param _process New process
+     */
     @Override
     public void setProcess(Slave _process) {
         this._process = _process;
     }
 
+    /**
+     * Starts the Station
+     */
     @Override
     public void startContainer() {
         if (!_moving) {
@@ -145,6 +227,9 @@ public class AssemblyStation implements PieceContainer {
         _moving = true;
     }
 
+    /**
+     * Stops the Station
+     */
     @Override
     public void stopContainer() {
         if (_moving) {
@@ -153,6 +238,10 @@ public class AssemblyStation implements PieceContainer {
         _moving = false;
     }
 
+    /**
+     * Updates the GUI with the pieces contained in the station
+     * @param piece Piece to update
+     */
     private void updatePosition(Piece piece) {
         if (piece.getType().equals(PieceType.assembly)) {
             piece.setGuiPosition(Constants.ASSEMBLY_STATION_GEAR_POSITION);
@@ -163,6 +252,9 @@ public class AssemblyStation implements PieceContainer {
         }
     }
 
+    /**
+     * @return TRUE if the station is currently working or not
+     */
     public boolean isActuating() {
         return _actuating;
     }
--- trunk/flexible-manufacturing-cell/src/Element/Station/QualityControlStation.java
+++ trunk/flexible-manufacturing-cell/src/Element/Station/QualityControlStation.java
@@ -1,6 +1,4 @@
-/*
- * This class checks the quality of pieces.
- */
+
 package Element.Station;
 
 import Automaton.Slaves.Slave;
@@ -15,16 +13,52 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This class includes the functionality to check if a piece is right or wrong
+ * @author Echoplex
+ */
 public class QualityControlStation implements PieceContainer {
 
+    /**
+     * Time to process the quality control
+     */
     private int _qualityTime;
+    
+    /**
+     * Quality Control Simulation Success Rate
+     * @see MasterConfigurationData
+     */
     private int _sucessRate = 60;
+    
+    /**
+     * Lits of pieces
+     */
     protected List<Piece> _pieces;
+    
+    /**
+     * Identifier
+     */
     private int _id;
+    
+    /**
+     * Process that makes the callback
+     */
     protected Slave _process;
+    
+    /**
+     * Shows if the station is active
+     */
     protected boolean _moving;
+    
+    /**
+     * Shows if the station is working
+     */
     private boolean _actuating;
 
+    /**
+     * Constructor of the class
+     * @param id Identifier
+     */
     public QualityControlStation(int id) {
         _id = id;
         _moving = false;
@@ -32,10 +66,17 @@ public class QualityControlStation implements PieceContainer {
         _pieces = Collections.synchronizedList(new ArrayList<Piece>());
     }
 
+    /**
+     * Run method
+     */
     @Override
     public synchronized void run() {
     }
 
+    /**
+     * Function that checks the quality of the piece loaded in the station
+     * @return TRUE if everything went good, FALSE in other case
+     */
     public boolean checkQuality() {
         if (_pieces.size() == 1) {
 
@@ -59,41 +100,71 @@ public class QualityControlStation implements PieceContainer {
         } else {
             return false;
         }
-
     }
 
+    /**
+     * @return The time to process the qualityControl
+     */
     public int getQualityTime() {
         return _qualityTime;
     }
 
+    /**
+     * Sets a new value for the quality control process
+     * @param qualityTime New value for the process time
+     */
     public void setQualityTime(int qualityTime) {
         this._qualityTime = qualityTime;
     }
 
+    /**
+     * @return The Success rate
+     */
     public int getSucessRate() {
         return _sucessRate;
     }
 
+    /**
+     * Sets a new value for the success rate
+     * @param sucessRate New success rate value - expected 0 - 100
+     */
     public void setSucessRate(int sucessRate) {
         this._sucessRate = sucessRate;
     }
 
+    /**
+     * Adds a piece to the station
+     * @param p Piece to add
+     */
     public void addPiece(Piece p) {
         _pieces.add(p);
         updatePosition(p);
     }
 
+    /**
+     * Adds a sensor to the station
+     * @param s Sensor to add
+     */
     public void addSensor(Sensor s) {
     }
 
+    /**
+     * @return The list of pieces contained in teh station
+     */
     public List<Piece> getPieces() {
         return _pieces;
     }
 
+    /**
+     * @return Status of the station
+     */
     public boolean isMoving() {
         return _moving;
     }
 
+    /**
+     * Removes the last piece added to the station
+     */
     public void removeLastPiece() {
         if (_pieces.isEmpty()) {
             Logger.getLogger(ConveyorBelt.class.getName()).log(Level.SEVERE, "Assembly station with id {0}: unable to remove last element", _id);
@@ -102,30 +173,51 @@ public class QualityControlStation implements PieceContainer {
         _pieces.remove(0);
     }
 
+    /**
+     * @return The Identifier
+     */
     @Override
     public int getId() {
         return _id;
     }
 
+    /**
+     * Sets a new value for the identifier
+     * @param id New Identifier
+     */
     @Override
     public void setId(int id) {
         _id = id;
     }
 
+    /**
+     * Sets a new list of pieces contained in the station
+     * @param pieces List of pieces
+     */
     public void setPieces(List<Piece> pieces) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * @return Process linked to the station
+     */
     @Override
     public Slave getProcess() {
         return _process;
     }
 
+    /**
+     * Sets the process linked to the station
+     * @param _process New linked process
+     */
     @Override
     public void setProcess(Slave _process) {
         this._process = _process;
     }
 
+    /**
+     * Starts the station
+     */
     @Override
     public void startContainer() {
         if (!_moving) {
@@ -134,6 +226,9 @@ public class QualityControlStation implements PieceContainer {
         _moving = true;
     }
 
+    /**
+     * Stops the station
+     */
     @Override
     public void stopContainer() {
         if (_moving) {
@@ -142,11 +237,17 @@ public class QualityControlStation implements PieceContainer {
         _moving = false;
     }
 
+    /**
+     * Updates the GUI Position of a piece
+     * @param piece Piece to update in GUI
+     */
     private void updatePosition(Piece piece) {
         piece.setGuiPosition(Constants.QUALITY_STATION_CENTER_POSITION);
     }
     
-    
+    /**
+     * @return The current working status of the station
+     */
     public boolean isActuating() {
         return _actuating;
     }
